d082ef9efb0f78b4b74f193a5789df4f73ac73a3
Luis e Laerte <laerte.guimaraes@live.com>
2019-02-21 22:03:00 -0300

    Implementa envio de imagens
    Co-authored-by: Luis <luis.arantes.sp@gmail.com>

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,7 @@ class Recipe < ApplicationRecord
   validates :cook_time, numericality: {other_than: 0, message: 'O tempo de preparo não pode ser igual a 0 (zero)!'}
   validates :cook_time, numericality: {greater_than: 0, message: 'O tempo de preparo não pode ser negativo!'}
   
+  has_one_attached :photo 
   
   def cook_time_min
     "#{cook_time} minutos"
