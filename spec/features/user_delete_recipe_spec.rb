73a7b62221fc018a4c12e5925eccce1d2555608b
Luis e Laerte <laerte.guimaraes@live.com>
2019-02-25 21:10:22 -0300

    Adiciona scenario na feature Delete Recipes
    Co-authored-by: Luis <luis.arantes.sp@gmail.com>

--- spec/features/user_delete_recipe_spec.rb
+++ spec/features/user_delete_recipe_spec.rb
@@ -18,4 +18,27 @@ feature 'User delete recipe' do
     expect(current_path).to eq(root_path)
   end
 
+  scenario 'with one or more recipes' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    recipe1 = Recipe.create(title: 'Bolo de Cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    recipe2 = Recipe.create(title: 'Bolo sem Cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 30, ingredients: 'Farinha, açucar',
+                  cook_method: 'Corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    visit root_path
+    click_on recipe1.title
+    click_on 'Deletar'   
+    
+    expect(Recipe.count).to eq(1)
+    expect(current_path).to eq(root_path)
+    expect(page).to have_css('h1', text: recipe2.title)
+    expect(page).not_to have_css('h1', text: recipe1.title)
+  end
+
 end
